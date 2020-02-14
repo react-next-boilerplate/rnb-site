284b7457bcdeaebb357cfec328c87a837670aa0a
Jorge Luis Calleja <jorgelca1205@gmail.com>
2020-02-13 20:54:14 -0600

    fix: meta name and twitter

--- src/components/SocialMeta/index.js
+++ src/components/SocialMeta/index.js
@@ -6,13 +6,19 @@ import Head from 'next/head';
 function SocialMeta({ title, description, image, url, keywords }) {
   return (
     <Head>
-      <meta name="twitter:site" content="@zeithq" />
-      <meta name="twitter:card" content={image ? 'summary_large_image' : 'summary'} />
       {title && <meta name="og:title" content={title} />}
+      <meta name="og:site_name" content="React Next Boilerplate" />
       {url && <meta name="og:url" content={url} />}
       {description && <meta name="description" content={description} />}
       {description && <meta name="og:description" content={description} />}
-      {image && <meta name="og:image" content={`https://www.reactnextboilerplate.com/${image}`} />}
+      <meta name="og:type" content="website" />
+      {image && <meta name="og:image" content={`https://www.reactnextboilerplate.com${image}`} />}
+      <meta name="twitter:site" content="@RNBoilerplate" />
+      {image && <meta name="twitter:card" content={image ? 'summary_large_image' : 'summary'} />}
+      {url && <meta name="twitter:url" content={url} />}
+      {title && <meta name="twitter:title" content={title} />}
+      {description && <meta name="twitter:description" content={description} />}
+      {image && <meta name="twitter:image" content={`https://www.reactnextboilerplate.com${image}`} />}
       {keywords && <meta name="keywords" content={keywords} />}
     </Head>
   );
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,7 +9,7 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           <link rel="apple-touch-icon" sizes="180x180" href="/static/favicon/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon/favicon-32x32.png" />
