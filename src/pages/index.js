d0e490a03dfed6e3aabcde5b5cd67de972e37390
Jorge Luis Calleja <jorgelca1205@gmail.com>
2020-02-13 20:04:03 -0600

    feat: Add component SocialMeta

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import React from 'react';
 
 import useSWR from 'swr';
@@ -8,6 +9,7 @@ import GettingStarted from '../components/GettingStarted';
 import Features from '../components/Features';
 import Contributing from '../components/Contributing';
 import Media from '../components/Media';
+import SocialMeta from '../components/SocialMeta';
 
 import { request } from '../utils/request';
 
@@ -16,6 +18,14 @@ function IndexPage() {
 
   return (
     <>
+      <SocialMeta
+        image="/static/images/home.png"
+        title="React Next Boilerplate"
+        url="https://reactnextboilerplate.com/"
+        description="A basis for reducing the configuration of your projects with Next.js, best development practices and popular libraries in the developer community."
+        keywords="react, nextjs, boilerplate, javascript, redux, redux-saga, immer"
+      />
+
       <Header version={data?.name} />
 
       <Banner />
