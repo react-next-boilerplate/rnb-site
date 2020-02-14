827364fcb45dcac56c4c1212235c14a6c29824a7
Jorge Luis Calleja <jorgelca1205@gmail.com>
2020-02-13 21:05:27 -0600

    fix: meta name

--- src/components/SocialMeta/index.js
+++ src/components/SocialMeta/index.js
@@ -6,20 +6,22 @@ import Head from 'next/head';
 function SocialMeta({ title, description, image, url, keywords }) {
   return (
     <Head>
-      {title && <meta name="og:title" content={title} />}
-      <meta name="og:site_name" content="React Next Boilerplate" />
-      {url && <meta name="og:url" content={url} />}
+      {title && <meta name="title" content={title} />}
       {description && <meta name="description" content={description} />}
-      {description && <meta name="og:description" content={description} />}
-      <meta name="og:type" content="website" />
-      {image && <meta name="og:image" content={`https://www.reactnextboilerplate.com${image}`} />}
-      <meta name="twitter:site" content="@RNBoilerplate" />
-      {image && <meta name="twitter:card" content={image ? 'summary_large_image' : 'summary'} />}
-      {url && <meta name="twitter:url" content={url} />}
-      {title && <meta name="twitter:title" content={title} />}
-      {description && <meta name="twitter:description" content={description} />}
-      {image && <meta name="twitter:image" content={`https://www.reactnextboilerplate.com${image}`} />}
-      {keywords && <meta name="keywords" content={keywords} />}
+      {title && <meta property="og:title" content={title} />}
+      <meta property="og:site_name" content="React Next Boilerplate" />
+      {url && <meta property="og:url" content={url} />}
+      {description && <meta property="description" content={description} />}
+      {description && <meta property="og:description" content={description} />}
+      <meta property="og:type" content="website" />
+      {image && <meta property="og:image" content={`https://www.reactnextboilerplate.com${image}`} />}
+      <meta property="twitter:site" content="@RNBoilerplate" />
+      {image && <meta property="twitter:card" content={image ? 'summary_large_image' : 'summary'} />}
+      {url && <meta property="twitter:url" content={url} />}
+      {title && <meta property="twitter:title" content={title} />}
+      {description && <meta property="twitter:description" content={description} />}
+      {image && <meta property="twitter:image" content={`https://www.reactnextboilerplate.com${image}`} />}
+      {keywords && <meta property="keywords" content={keywords} />}
     </Head>
   );
 }
