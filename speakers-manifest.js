556e36c9ff03e4f15dd621a8aee9e0fe51e224f5
Jorge Luis Calleja <jorgelca1205@gmail.com>
2020-05-09 16:19:59 -0500

    fead: base speakers-manifest.js

---
+++ speakers-manifest.js
@@ -0,0 +1,29 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable max-len */
+
+/*
+    name: 'name',
+    place: 'place',
+    role: 'place',
+    workplace: 'workplace',
+    resume: 'resume',
+    src: 'avatar',
+    twitter: 'user',
+*/
+
+export const mapping = {};
+
+const wootsbot = {
+  name: 'Jorge L. Calleja A.',
+  place: 'México',
+  role: 'Frontend UI',
+  workplace: 'GBM',
+  resume:
+    'Doing this "n" that. I am a JavaScript developer, living in Mexico. I am a co-author of React Next Boilerplate and a builder of things. open source enthusiast. I am a passionate photographer.',
+  src: '/static/images/speakers/wootsbot.jpg',
+  twitter: 'wootsbot',
+};
+
+const speakers = Object.values(mapping);
+
+export { wootsbot, speakers };
